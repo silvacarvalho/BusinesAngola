b6e341c9a28ca36c8415993bc1bddd35ad5fa032
Rafael S. Carvalho <silva_carvalho20@hotmail.com>
2015-04-18 21:26:32 -0300

    Alterando a linguagem padrão de boas vindas para PT-BR... \n Adicionando uma barra na chamada da subpágina na tela inicial.

---
+++ app/language/pt-BR/welcome.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    //Welcome
+    'welcome_text'    => 'Bem Vindo!',
+    'welcome_message' => '
+                                Olá, este é o controller de boas vindas! <br/>
+                                Este conteúdo pode ser alterado em: <code>/app/views/welcome/welcome.php</code>
+	',
+    //Subpage
+    'subpage_text'    => 'Subpage',
+    'subpage_message' => '
+		Olá, este é o controller de boas vindas de Sub Pagina! <br/>
+		Este conteúdo pode ser alterado em: <code>/app/views/welcome/subpage.php</code>
+	',
+    //Button
+    'open_subpage'    => 'Abrir Subpágina',
+    'back_home'       => 'Home',
+];
