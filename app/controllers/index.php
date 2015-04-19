da69656fbf365df72e462beae3f386a47c899d6e
Rafael S. Carvalho <silva_carvalho20@hotmail.com>
2015-04-18 23:22:46 -0300

    Retirando arquivos desnecessários. Alterando o template padrão.

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -35,4 +35,17 @@ class Index extends \core\Controller
         View::render('index/index', $data);
         View::rendertemplate('footer', $data);
     }
+
+
+    /**
+     * Define Subpage page title and load template files
+     */
+    public function subpage() {
+        $data['title'] = $this->language->get('subpage_text');
+        $data['welcome_message'] = $this->language->get('subpage_message');
+
+        View::rendertemplate('header', $data);
+        View::render('welcome/subpage', $data);
+        View::rendertemplate('footer', $data);
+    }
 }
